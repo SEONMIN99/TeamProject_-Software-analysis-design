ce4f1e41a6e902375757fac56e110ac4390726e4
SEONMIN99 <seonmin99@naver.com>
2019-11-28 16:45:22 +0900

    클래스 생성

---
+++ Loan.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Loan here.
+ *
+ * @author (2018315040 김선민, 2018315034 정철우, 2018315026 정대기, 2018315057 리얼윈 케이토)
+ * @version (2019/11/28)
+ */
+public class Loan
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Loan
+     */
+    public Loan()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
