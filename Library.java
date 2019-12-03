212d1bc2b62f743df28e15895094a42bd0aadb24
dackyy <eorl4940@naver.com>
2019-12-03 22:33:39 +0900

    대출가능한책과 대출중인책 메소드 구현

--- Library.java
+++ Library.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.TreeSet;
+import java.util.Iterator;
 /**
  * Write a description of class Library here.
  *
@@ -36,7 +37,7 @@ public class Library
         Borrower borrower = new Borrower();
         borrower.setName(name);
         if(Borrowers.contains(name) == false)
-        {		
+        {       
             Borrowers.add(borrower.getName());
 
             //System.out.println(Borrowers);
@@ -56,7 +57,7 @@ public class Library
         book.setTitle(title);
         book.setAuthor(author);
         if(Books.contains(catalogueNumber) == false)
-        {		
+        {       
             Books.add(catalogueNumber);
             //System.out.println(Books);
             System.out.println(book.getTitle() + "책이 등록되었습니다.");
@@ -70,13 +71,32 @@ public class Library
     //3. 대출 가능한 책들을 화면에 출력한다.
     public void DisplayBooksForLoan()
     {
+        Iterator iter = Books.iterator();
+        while(iter.hasNext()){
+            Book book = (Book)iter.next();
+            if((book.getBorrower()) == null){
+                book.display();
+            }
+            else{
+                
+            }
+        }
 
     }
 
     //4. 대출 중인 책들을 화면에 출력한다.
     public void DisplayBooksOnLoan()
     {
-
+        Iterator iter = Books.iterator();
+        while(iter.hasNext()){
+            Book book = (Book)iter.next();
+            if((book.getBorrower()) == null){
+                
+            }
+            else{
+                book.display();
+            }
+        }
     }
 
     //5. 책을 대출한다.
